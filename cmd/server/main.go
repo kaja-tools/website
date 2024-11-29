97b7aa4eaacfed84180b089deb090157b1fc2b60
Tomas Vesely <448809+wham@users.noreply.github.com>
2024-11-29 00:06:24 -0800

    Added basic stuff

---
+++ cmd/server/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "https://github.com", http.StatusFound)
+	})
+
+	fmt.Println("Server starting on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Printf("Server error: %v\n", err)
+	}
+}
